73f24e6a3fa80a47a0d52294e143498c5d36b02d
Seung-0208 <esybd02@naver.com>
2025-11-01 18:27:56 +0900

    [BE] 코드 합치기

---
+++ backend/src/main/java/com/capstone/samadhi/record/dto/RecordResponse.java
@@ -0,0 +1,33 @@
+package com.capstone.samadhi.record.dto;
+
+import com.capstone.samadhi.record.entity.Record;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record RecordResponse(
+        Long id,
+        String dateTime,
+        Duration workingout_time,
+        String youtube_url,
+        int total_score,
+        List<TimeLineResponse> timelines
+) {
+
+    public static RecordResponse from(Record record) {
+
+        List<TimeLineResponse> timelines = record.getTimeLineList().stream()
+                .map(TimeLineResponse::from) // TimeLineResponse의 정적 메소드 사용
+                .collect(Collectors.toList());
+
+        return new RecordResponse(
+                record.getId(),
+                record.getCreatedAt().toString(),
+                record.getWorkingout_time(),
+                record.getYoutube_url(),
+                record.getTotal_score(),
+                timelines
+        );
+    }
+}
---
+++ backend/src/main/java/com/capstone/samadhi/record/service/RecordService.java
@@ -0,0 +1,51 @@
+package com.capstone.samadhi.record.service;
+
+import com.capstone.samadhi.common.ResponseDto;
+import com.capstone.samadhi.record.dto.RecordRequest;
+import com.capstone.samadhi.record.dto.RecordResponse;
+import com.capstone.samadhi.record.entity.Record;
+import com.capstone.samadhi.record.repository.RecordRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly=true)
+@RequiredArgsConstructor
+public class RecordService {
+    private final RecordRepository recordRepository;
+
+    @Transactional
+    public ResponseDto<RecordResponse> save(RecordRequest request) {
+        Record record = request.toEntity();
+        Record savedRecord = recordRepository.save(record);
+        RecordResponse response = RecordResponse.from(savedRecord);
+        return new ResponseDto<>(true, response);
+    }
+
+    public ResponseDto<RecordResponse> findById(Long id) {
+        Record record = recordRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Record not found"));
+        /*
+        * user 에러처리
+        * if(!record.getUser().getId().equals()){
+        *   throw new AccessDeniedException("접근 권한이 없습니다.");
+        * }
+        * */
+        RecordResponse response = RecordResponse.from(record);
+        return new ResponseDto<>(true, response);
+    }
+//    public ResponseDto<List<RecordResponse>> findByUser(Pageable pageable) {
+//
+//        User user = userService.findUserByEmail(userEmail)
+//                .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다: " + userEmail));
+//
+//        Page<Record> recordPage = recordRepository.findByUser(user, pageable);
+//
+//        List<RecordResponse> responseList = recordPage.getContent().stream()
+//                .map(RecordResponse::from)
+//                .collect(Collectors.toList());
+//
+//        return new ResponseDto<>(true, responseList);
+//    }
+}
